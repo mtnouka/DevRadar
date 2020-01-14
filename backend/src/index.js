a76cc94b64c4d97246139cfc8857d834d821b6dd
Matheus Souza <mtheus.csouza@gmail.com>
2020-01-14 15:49:25 -0300

    Criação do backend da aplicação para suportar os frontends web e mobile

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://omnistack:omnistack@omnistack-shard-00-00-n7ixr.mongodb.net:27017,omnistack-shard-00-01-n7ixr.mongodb.net:27017,omnistack-shard-00-02-n7ixr.mongodb.net:27017/week10?ssl=true&replicaSet=OmniStack-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query params: req.query (Filtros, ordenação, paginação, ...)
+// Route params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
