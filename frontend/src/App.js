72d35c5453cd9dfb8a2c57e6f653959bcf58a482
mtnouka <mtheus.csouza@gmail.com>
2020-01-27 16:08:15 -0300

    Comunicando com o backend para exibição e cadastro de devs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,92 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 // Componente: bloco isolado de HMTL, CSS e JS, o qual não interfere no restante da aplicação
 // Propriedade: Informações que um componente PAI passa para o componente FILHO
 // Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/42550653?s=400&u=8adc5c5d4e1704f60fbf67d09760e3c2bb3c5e80&v=4" alt="Matheus Souza" />
-              <div className="user-info">
-                <strong>Matheus Souza</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Junior Backend Developer</p>
-            <a href="https://github.com/mtnouka">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/42550653?s=400&u=8adc5c5d4e1704f60fbf67d09760e3c2bb3c5e80&v=4" alt="Matheus Souza" />
-              <div className="user-info">
-                <strong>Matheus Souza</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Junior Backend Developer</p>
-            <a href="https://github.com/mtnouka">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/42550653?s=400&u=8adc5c5d4e1704f60fbf67d09760e3c2bb3c5e80&v=4" alt="Matheus Souza" />
-              <div className="user-info">
-                <strong>Matheus Souza</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Junior Backend Developer</p>
-            <a href="https://github.com/mtnouka">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/42550653?s=400&u=8adc5c5d4e1704f60fbf67d09760e3c2bb3c5e80&v=4" alt="Matheus Souza" />
-              <div className="user-info">
-                <strong>Matheus Souza</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Junior Backend Developer</p>
-            <a href="https://github.com/mtnouka">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
